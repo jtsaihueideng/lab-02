04d4ab60e2e01a98c98d4b6b0c4f587a2f6e64ea
Julie Deng <julieden@usc.edu>
2023-01-27 09:18:56 -0800

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -6,11 +6,23 @@ Establish a socket connection -> send a short message -> get a message back -> t
 use python "input->" function, enter a line of a few letters, such as "abcd"
 """
 import socket
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 10000
 
 def main():
+
+
+
     # TODO: Create a socket and connect it to the server at the designated IP and port
     # TODO: Get user input and send it to the server using your TCP socket
     # TODO: Receive a response from the server and close the TCP connection
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.connect((HOST, PORT))
+      inp = input("Enter your message: ")
+      arr = bytes(inp,'utf-8')  
+      s.sendall(arr) 
+      data = s.recv(1024) 
+      print(data) 
     pass
 
 
